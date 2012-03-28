3407dbcbbdf64bb1eccac30af2d881d52f5c4edf
Petyo Ivanov <underlog@gmail.com>
2012-03-28 10:55:13 +0300

    Fix all the problems!

--- child-browser/reddithotness/www/app.js
+++ child-browser/reddithotness/www/app.js
@@ -1,90 +1,97 @@
 var app = (function() {
-	
-	var reddit_base = "http://reddit.com";
-	
-	var pub = {};
-	
-	var tmp = new kendo.data.DataSource({
-		transport: {
-			read: "test.json"
-		},
-		schema: {
-			data: "data.children",
-			fields: {
-				title: "data.title",
-				url: "data.url",
-				points: "data.score"
-			}
-		},
-		sort: { field: "data.score", dir: "desc" }
-	});
 
-	var reddit = new kendo.data.DataSource({
-		transport: {
-			read: {
-				url: "http://reddit.com/r/programming.json",
-				dataType: "json"
-			}
-		},
-		schema: {
-			data: "data.children",
-			fields: {
-				title: "data.title",
-				url: "data.url",
-				score: "data.score"
-			}
-		},
-		sort: { field: "data.score", dir: "desc" }
-	});
-	
-	var reddits = new kendo.data.DataSource({
-		transport: {
-			read: "reddits.json"
-		},
-		schema: {
-			data: "data.children",
-			fields: {
-				name: "data.display_name",
-				url: reddit_base + "data.url" + ".json",
-				title: "title"
-			}
-		},
-		group: {
-			
-		}
-	})
+    var reddit_base = "http://www.reddit.com";
+
+    var pub = {};
+
+    var tmp = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "test.json",
+                dataType: "json"
+            }
+        },
+        schema: {
+            data: "data.children",
+            fields: {
+                title: "data.title",
+                url: "data.url",
+                points: "data.score"
+            }
+        },
+        sort: { field: "data.score", dir: "desc" }
+    });
+
+    var reddit = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "http://www.reddit.com/r/programming.json",
+                dataType: "json"
+            }
+        },
+        schema: {
+            data: "data.children",
+            fields: {
+                title: "data.title",
+                url: "data.url",
+                score: "data.score"
+            }
+        },
+        sort: { field: "data.score", dir: "desc" }
+    });
+
+    var reddits = new kendo.data.DataSource({
+        transport: {
+            read: "reddits.json"
+        },
+        schema: {
+            data: "data.children",
+            fields: {
+                name: "data.display_name",
+                url: reddit_base + "data.url" + ".json",
+                title: "title"
+            }
+        },
+        group: {
+
+        }
+    })
 
 
-	pub.init = function() {
-		
-		// create the kendo ui mobile application
-		var kendo_app = new kendo.mobile.Application();
-	
-		// create the listview
-		$("#popular_news").kendoMobileListView({
-			dataSource: tmp,
-			template: kendo.template($("#template").html()),
-			style: "inset"
-			/*click: function(e) {
+    pub.initPopularViews = function() {
+        // create the listview
+        $("#popular_news").kendoMobileListView({
+            dataSource: tmp,
+            template: kendo.template($("#template").html()),
+            pullToRefresh: true,
+            style: "inset",
+            click: function(e) {
+                console.log(e);
+            }
+        });
+    }
 
-			}*/
-		});
-		
-		$("#select-a-reddit").kendoMobileListView({
-			dataSource: reddits,
-			template: "${name}"
-			style: "inset"
-		});	
-	}
-	
+    pub.initSelectReddit = function() {
+        $("#select-a-reddit").kendoMobileListView({
+            dataSource: tmp,
+            template: "${name}",
+            style: "inset"
+        });
+    }
 
-	pub.goToUrl = function(sender) {
-		var url = $(sender).data("url");
-		var cb = ChildBrowser.install();
-		if(cb != null)
-			window.plugins.childBrowser.showWebPage(url);
-	}
-	
-	return pub;
-	
+    pub.init = function() {
+        // create the kendo ui mobile application
+        var kendo_app = new kendo.mobile.Application();
+    }
+
+
+    pub.goToUrl = function(sender) {
+        var url = $(sender).data("url");
+        var cb = ChildBrowser.install();
+        if(cb != null)
+            window.plugins.childBrowser.showWebPage(url);
+    }
+
+    return pub;
+
 })();
